570dea21f829327a9ca322cac8466719c145516b
shuvoroy10 <p1shuvoroy@gmail.com>
2025-11-14 22:05:04 +0600

    Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Outlet></Outlet>
+    </>
+  )
+}
+
+export default App
---
+++ src/Route/Router.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import App from '../App';
+import ErrorPage from '../Pages/ErrorPage.jsx/ErrorPage';
+import Home from '../Pages/Home/Home';
+import Service from '../Pages/Service/Service';
+import MyProfile from '../Pages/MyProfile/MyProfile';
+
+const Router = createBrowserRouter([
+    {
+        path: '/',
+        errorElement: <ErrorPage/>,
+        Component: App,
+        children:[
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: 'service',
+                Component: Service
+            },
+            {
+                path: 'my-profile',
+                Component: MyProfile
+            }
+        ]
+    }
+])
+
+export default Router;
