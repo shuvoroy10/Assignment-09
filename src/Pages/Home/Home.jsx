69dd8bf31cc491f1fde677be243c19a710be9520
shuvoroy10 <p1shuvoroy@gmail.com>
2025-11-14 23:41:41 +0600

    added slider to homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { FreeMode, Pagination } from "swiper/modules";
 
 const Home = () => {
-    return (
-        <div>
-            Home page
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{ clickable: true }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper my-5"
+      >
+        <SwiperSlide><img src="https://i.ibb.co.com/HfVsNWgP/Adobe-Stock-314342216-scaled.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/TMdMLcHH/dog-coat-1024x717.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/Z6L8QMR2/jeremy-bishop-ph5-Ayj-Flt-QU-unsplash.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/7tkwFfNW/elisa-pitkanen-cwk-Rd-Es-ZYn0-unsplash.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/fYRtH0Jh/elisa-pitkanen-rf-ANqv-OV-0-unsplash.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/yctYRvJ6/1.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/8DvXcvPG/marie-michele-bouchard-x-Ez-Irv-JX6-LQ-unsplash.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/Rkm9qc4d/ybag-media-se3p-Se-T0md-M-unsplash.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co.com/yctSH7sF/Dog-Sweater-Alice-Neal-Love-Knitting-5c649c6646e0fb0001f09122.jpg" alt="" /></SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Home;
